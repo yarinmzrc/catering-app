f0dededb43b462f77dffcfe7b1759858d7ff1608
yarinmzrc <yarinmzrc@gmail.com>
2025-12-15 22:14:22 +0200

    add login form

---
+++ src/app/admin/login/layout.tsx
@@ -0,0 +1,7 @@
+export default function AdminLoginLayout({
+  children,
+}: React.PropsWithChildren) {
+  return (
+    <div className="container mx-auto flex justify-center p-6">{children}</div>
+  )
+}
--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,32 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 export default function AdminLoginPage() {
   return (
-    <div>
-      <h1>Admin Login</h1>
-    </div>
+    <>
+      <Card className="w-120">
+        <CardHeader>
+          <CardTitle>
+            <h1 className="text-4xl font-bold">Admin Login</h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form action="/api/admin/login" method="post" className="space-y-8">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" name="email" required />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" name="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </>
   )
 }
